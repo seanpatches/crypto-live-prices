a43b5dd331e3853d8492572b88e0085e0e75a308
Sean Nelson <smnelsonn@gmail.com>
2023-06-20 21:52:13 -0600

    start styling price view

--- Views/PricesScreen.tsx
+++ Views/PricesScreen.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useCallback, useEffect, useState } from 'react';
-import { AppState, StyleSheet, Text, View } from 'react-native';
+import { AppState, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { fetchPriceByCurrency } from '../services/requests';
 import { createTickerWebsocket } from '../sockets/websockets';
 import { CryptoPrices, ChangingPrices, CurrencyTypes, MessageTypes } from '../types';
@@ -100,27 +99,55 @@ const PricesScreen = () => {
     
 
   return (
-    <View style={styles.container}>
+    <View style={styles.pricesContainer}>
       {prices && (
-        <View>
-          <Text>BTC: {prices.BTC}</Text>
-          <Text>ETH: {prices.ETH}</Text>
-          <Text>FLOW: {prices.FLOW}</Text>
-          <Text>ALGO: {prices.ALGO}</Text>
-        </View>
+        <ScrollView style={styles.pricesList}>
+          <View style={styles.row}>
+            <Image source={{ uri: "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"}} style={styles.rowImage} />
+            <Text style={styles.priceText}>BTC: {prices.BTC}</Text>
+          </View>
+          <View style={styles.row}>
+            <Image source={{ uri: "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"}} style={styles.rowImage} />
+            <Text style={styles.priceText}>ETH: {prices.ETH}</Text>
+          </View>
+          <View style={styles.row}>
+            <Image source={{ uri: "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"}} style={styles.rowImage} />
+            <Text style={styles.priceText}>FLOW: {prices.FLOW}</Text>
+          </View>
+          <View style={styles.row}>
+            <Image source={{ uri: "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"}} style={styles.rowImage} />
+            <Text style={styles.priceText}>ALGO: {prices.ALGO}</Text>
+          </View>
+        </ScrollView>
       )}
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pricesContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  pricesList: {
+    width: "100%",
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderTopWidth: 1,
+  },
+  rowImage: {
+    width: 50,
+    height: 50,
+  },
+  priceText: {
+    paddingLeft: 20,
+    width: 200,
+    fontSize: 22
+  }
 });
 
 export default PricesScreen;
